5bc605564359a25ec441e1fa3bdd926d95f3d07f
dandyrow <daniel@daniellowry.co.uk>
2022-03-11 15:29:24 +0000

    Added functions for working with JWTs

---
+++ src/helper/tokens.ts
@@ -0,0 +1,31 @@
+import { Response } from 'express';
+import { sign } from 'jsonwebtoken';
+import { User } from '../entity/User';
+
+export function createAccessToken(user: User) {
+    return sign(
+        {
+            userId: user.id,
+            username: user.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET!,
+        { expiresIn: '10m' },
+    );
+}
+
+export function createRefreshToken(user: User) {
+    return sign(
+        {
+            userId: user.id,
+            tokenVersion: user.tokenVersion,
+        },
+        process.env.REFRESH_TOKEN_SECRET!,
+        { expiresIn: '1y' },
+    );
+}
+
+export function sendRefreshToken(res: Response, token: string) {
+    res.cookie('chocolate', token, {
+        httpOnly: true,
+    });
+}
